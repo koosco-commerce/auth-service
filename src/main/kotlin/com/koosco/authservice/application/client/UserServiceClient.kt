bba431915b5986018c91f83e3a4289ca704dac39
Koo <koosco136@gmail.com>
2025-11-30 21:48:55 +0900

    feat: remove client for user service

--- src/main/kotlin/com/koosco/authservice/application/client/UserServiceClient.kt
+++
@@ -1,7 +0,0 @@
-package com.koosco.authservice.application.client
-
-import com.koosco.authservice.infra.client.dto.UserInfoResponse
-
-interface UserServiceClient {
-    fun getUserInfo(userId: Long): UserInfoResponse
-}
--- src/main/kotlin/com/koosco/authservice/infra/client/dto/UserInfoResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.koosco.authservice.infra.client.dto
-
-data class UserInfoResponse(
-    val id: Long,
-    val email: String,
-    val name: String,
-    val phone: String?,
-    val roles: List<String>,
-)
