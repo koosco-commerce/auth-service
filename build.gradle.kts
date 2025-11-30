66455f35b0f7583a1e6860cbf04183dee9d3d3b9
Teahyung Koo <150074724+koosco@users.noreply.github.com>
2025-11-30 22:38:28 +0900

    Fix/local env mariadb (#5)
    * fix: update application configuration for MariaDB and JWT settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
 
     // jpa
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 
     // flyway
